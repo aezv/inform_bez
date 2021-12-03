7747a64736a2abb995e95c12ec5eee8d242410fd
aezv <aezv.global@gmail.com>
2021-12-03 17:58:35 +0300

    Разные правки и добавление расшифровщика в L3

--- scripts/L3.js
+++ scripts/L3.js
@@ -1,15 +1,21 @@
 let key = document.getElementById('key');
 let text = document.getElementById('text');
 let button_key_gen = document.getElementById('button_key_gen');
-let button_start = document.getElementById('button_start');
+let button_start_encrypt = document.getElementById('button_start_encrypt');
+let button_start_decrypt = document.getElementById('button_start_decrypt');
 let button_clear = document.getElementById('button_clear');
 let count_symbols = document.getElementById('count_symbols');
+let edit_count_symbols = document.getElementById('edit_count_symbols');
 let key_table = document.getElementById('key_table');
-button_start.addEventListener('click', function () {
+button_start_encrypt.addEventListener('click', function () {
     let data_key = key.value.split(' ');
     let data_text = text.value.toUpperCase().split('');
+    let data_count_symbols = data_text.length;
+    data_text = data_text.filter((elem) => elem != ' ' && elem != '\n');
+    let data_edit_count_symbols = data_text.length;
     if (data_text.length % data_key.length == 0 && 2 <= data_key.length) {
-        count_symbols.innerText = 'Общее кол-во символов: ' + data_text.length;
+        count_symbols.innerText = 'Общее кол-во символов: ' + data_count_symbols + ' | ';
+        edit_count_symbols.innerText = 'Общее кол-во символов после преобразования: ' + data_edit_count_symbols;
         let data_text_encryption = new Array();
         for (let i = 0; i < data_key.length; i++)
             data_key[i] = Number(data_key[i]);
@@ -38,8 +44,45 @@ button_start.addEventListener('click', function () {
     }
 });
 
-button_key_gen.addEventListener('click', function () {
+button_start_decrypt.addEventListener('click', function(){
+    let data_key = key.value.split(' ');
     let data_text = text.value.toUpperCase().split('');
+    let data_count_symbols = data_text.length;
+    data_text = data_text.filter((elem) => elem != ' ' && elem != '\n');
+    let data_edit_count_symbols = data_text.length;
+    if (data_text.length % data_key.length == 0 && 2 <= data_key.length) {
+        count_symbols.innerText = 'Общее кол-во символов: ' + data_count_symbols + ' | ';
+        edit_count_symbols.innerText = 'Общее кол-во символов после преобразования: ' + data_edit_count_symbols;
+        let data_text_encryption = new Array();
+        for (let i = 0; i < data_key.length; i++)
+            data_key[i] = Number(data_key[i]);
+        for (let i = 0; i < data_text.length; i += data_key.length) {
+            for (let j = 0; j < data_key.length; j++)
+                data_text_encryption.push(data_text[i + data_key[j]]);
+        }
+        text.value = data_text_encryption.join('');
+
+        let data_key_table = '<tr><th>Индекс</th>';
+        for (let i = 0; i < data_key.length; i++)
+            data_key_table += '<td>' + i + '</td>';
+        data_key_table += '</tr><tr><th>Ключ</th>';
+        for (let i = 0; i < data_key.length; i++)
+            data_key_table += '<td>' + data_key[i] + '</td>';
+        data_key_table += '</tr>';
+        key_table.innerHTML = data_key_table;
+    }
+    else {
+        let multiples = new Array();
+        for (let i = 2; i <= data_text.length; i++) {
+            if (data_text.length % i == 0)
+                multiples.push(i);
+        }
+        key.value = 'Предполагается что ключ должен иметь одну из длин: ' + multiples.join(' ');
+    }
+});
+
+button_key_gen.addEventListener('click', function () {
+    let data_text = text.value.toUpperCase().split('').filter((elem) => elem != ' ' && elem != '\n');
     let multiples_array = new Array();
     for (let i = 2; i <= data_text.length; i++) {
         if (data_text.length % i == 0) {
@@ -60,5 +103,6 @@ button_clear.addEventListener('click', function () {
     key.value = '';
     text.value = '';
     count_symbols.innerText = '';
+    edit_count_symbols.innerText = '';
     key_table.innerHTML = '';
 });
